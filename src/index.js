1f7f7889f09977b734ee37d2b208f703dbba9d41
zach lewis <zlewis@clarityinnovates.com>
2022-08-23 12:57:49 -0500

    auth step 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
@@ -7,7 +8,12 @@ import { GlobalStyles } from './global-styles';
 ReactDOM.render(
 	<React.StrictMode>
 		<GlobalStyles />
-		<App />
+		<Auth0Provider
+			domain="dev-84vaxfa6.us.auth0.com"
+			clientId="yHdPgT52L5c2P4EB1zUDaXsZGUakzUjt"
+			redirectUri={window.location.origin}
+		>	<App />
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
